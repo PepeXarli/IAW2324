6a9ceb9d64c2e78f5a40bdc39a77e440f1a92f2d
José Carlos Navarro <josenavarro04@iesamachado.org>
2024-01-09 14:30:23 +0100

    pasando a tinker

--- 2º Trimestre/crud/gestion/conexion.php
+++ 2º Trimestre/crud/gestion/conexion.php
@@ -3,10 +3,10 @@
     $servername = "sql307.byetcluster.com";
     $username = "thsi_35748569";
     $password = "U?daGhYf";
-    $bd="dbname=thsi_35748569_bdpruebas";
+    $bd="thsi_35748569_bdpruebas";
 
     try {
-    $conn = new PDO("mysql:host=$servername;dbname=thsi_35748569_bdpruebas", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$bd", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Conectado con éxito a $servername con usuario $username y contraseña $password";
--- 2º Trimestre/crud/gestion/leer1fila.php
+++ 2º Trimestre/crud/gestion/leer1fila.php
@@ -1,6 +1,20 @@
 <?php
     //Conexión a base de datos mediante PDO
-    include "conecion.php";
+    include "conexion.php";
 
-    
+        // echo "SELECT * FROM usuarios WHERE id=1";
+        $sql = "SELECT * FROM usuarios WHERE id=1";
+        $result = $conn->query($sql);
+        
+        if ($result->rowCount() > 0) {
+            echo "<table><tr><th>ID</th><th>Name</th></tr>";
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+            echo "<tr><td>".$row["id"]."</td><td>".$row["username"]." ".$row["password"]."</td></tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "0 results";
+        }
+    $conn->close();
 ?>
