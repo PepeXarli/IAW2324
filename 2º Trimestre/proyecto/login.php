9152393f701a97b9fd1069ab00f00c146d32eb5b
José Carlos Navarro <josenavarro04@iesamachado.org>
2024-01-14 00:02:11 +0100

    login pa movil

--- 2º Trimestre/proyecto/login.php
+++ 2º Trimestre/proyecto/login.php
@@ -14,27 +14,32 @@
         </h1>
 
         <div>
-            <form action="#" method="post">
-            <p>Usuario:</p>
+            <form action="#" method="post" id="formu">
+            <label for="">Usuario</label>
             <input type="text" name='usuario' placeholder='Ej.:Manolito'> <br>
-            <p>Contraseña:</p>
+            <label for="">Contraseña</label>
             <input type="password" name='contrasena' placeholder='Ej.:sUp3rM4N0lit0'><br> <br>
-            <button type="submit" name='submit'>Login</button> 
+            <button name="submit" type="submit" class="myButton">Login</button>
             </form>
         </div>
+        <label for="" id="mensaje" class=''></label>
     </div>
 
-    <?php
+</body>
+</html>
+
+<?php
         //Conexión a base de datos mediante PDO
 
-        if(isset($_POST['submit'])){
+        if ($_SERVER['REQUEST_METHOD'] == "POST"){
 
             $usuario=htmlspecialchars($_POST['usuario']);
             $passwd=base64_encode(htmlspecialchars($_POST['contrasena']));
             
 
             if($usuario==null OR $passwd==null){
-                echo "Todos los campos son obligatorios";
+                echo "<script> document.getElementById('mensaje').innerHTML='Todos los campos son obligatorios'
+                document.getElementById('mensaje').className='error'</script>";
             } else{
 
                 $showMessages = false;
@@ -43,21 +48,20 @@
         
                 header("Content-type:text/html;charset=utf-8");
 
-                $comprobar = "SELECT * FROM usuproyecto where usuario='$usuario ' AND contrasena='$passwd'";
+                $comprobar = "SELECT * FROM usuproyecto where usuario='$usuario' AND contrasena='$passwd'";
                 $result = $conn->query($comprobar);
 
                 
                 if ($result->rowCount() > 0) {
                     // Mira si el usuario ha sido registrado
-                    echo "Inicio de sesión correcto";
+                    echo "<script> document.getElementById('mensaje').innerHTML='Inicio de sesión correcto';
+                    document.getElementById('mensaje').className='acierto'</script>";
         
                 } else {
-                    echo "Usuario o contraseña incorrecto";
+                    echo "<script> document.getElementById('mensaje').innerHTML='Usuario o contraseña incorrecto'
+                    document.getElementById('mensaje').className='error'</script>";
                 }
             $conn->close();
             }
         }
-    ?>
-
-</body>
-</html>
+?>
