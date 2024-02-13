b8dcc987c63252b9fff3108270edc1786b9cd265
José Carlos Navarro <josenavarro04@iesamachado.org>
2024-02-13 20:02:35 +0100

    delete ya seguro

--- 2º Trimestre/proyecto/delete.php
+++ 2º Trimestre/proyecto/delete.php
@@ -2,11 +2,22 @@
 <?php 
      if(isset($_GET['eliminar']))
      {
-         $id= htmlspecialchars($_GET['eliminar']);
-         $query = "DELETE FROM incidencias WHERE id = {$id}"; 
-         $delete_query= $conn->query($query);
-         // header("Location: home.php");
-         echo "<script>window.location='incidencias.php';</script>";
+        $id= htmlspecialchars($_GET['eliminar']);
+
+        $user = $_SESSION['usuario'];
+
+        
+
+        if($_SESSION['admin']!="Administrador"){
+            $query = "DELETE FROM incidencias WHERE id = {$id} AND user='$user'"; 
+        }else{
+            $query = "DELETE FROM incidencias WHERE id = {$id}"; 
+        }
+    
+
+        $delete_query= $conn->query($query);
+        // header("Location: home.php");
+        echo "<script>window.location='incidencias.php';</script>";
      }
 ?>
 <?php include "footer.php" ?>
