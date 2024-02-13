6f92eb6542359b24c177cd82955659d7f563b429
José Carlos Navarro <josenavarro04@iesamachado.org>
2024-02-13 10:15:06 +0100

    delete user un poco mejorao

--- 2º Trimestre/proyecto/deleteuser.php
+++ 2º Trimestre/proyecto/deleteuser.php
@@ -8,19 +8,21 @@
     if($_SESSION['admin']!="Administrador"){
         echo "<script> alert ('Debe ser admin para acceder a esta página') 
         window.location='http://pepe.thsite.top/proyecto/incidencias.php'</script>";
+    } else {
+
+        if(isset($_GET['eliminar']))
+        {
+            $usuario= htmlspecialchars($_GET['eliminar']);
+            $query = "DELETE FROM usuproyecto WHERE usuario = '{$usuario}'"; 
+            $delete_query= $conn->query($query);
+            // header("Location: home.php");
+            echo "<script>window.location='incidencias.php';</script>";
+        }
+
     }
 
 
 ?>
 
-<?php 
-     if(isset($_GET['eliminar']))
-     {
-         $usuario= htmlspecialchars($_GET['eliminar']);
-         $query = "DELETE FROM usuproyecto WHERE usuario = '{$usuario}'"; 
-         $delete_query= $conn->query($query);
-         // header("Location: home.php");
-         echo "<script>window.location='incidencias.php';</script>";
-     }
-?>
+
 <?php include "footer.php" ?>
